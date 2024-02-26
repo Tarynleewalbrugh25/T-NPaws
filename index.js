780b141715a78d22e0fd154f2da9c6ebc6de7059
Tarynleewalbrugh25 <walbrught@gmail.com>
2024-02-26 15:13:46 +0200

    fo

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('hello');
+// console.log('안녕하세요');
+import {userRouter} from './controller/UserController.js';
+import {productRouter} from './controller/ProductController.js';
+import cookieParser from "cookie-parser"
+import {errorHandling} from './middleware/ErrorHanding.js';
+import path from 'path'
+import cors from 'cors'
+import express  from 'express'
+import {config} from 'dotenv'
+config()
+const app = express()
+const port = +process.env.PORT || 5000
+//middleware
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+})
+app.use(
+    express.static('./static'),
+    express.json(),
+    express.urlencoded({
+        extended: true,
+    }),
+    cookieParser(),
+    cors()
+)
+app.get('^/$|/Paws&TnN', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './static/index.html'))
+})
+// app.use('/^')
+// app.use('^/$')
+app.use('/users', userRouter)
+app.use('/products', productRouter)
+app.use(errorHandling)
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
